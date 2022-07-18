5a2e95b9c10e81185c7b5ef608e0d6e12de3feff
Thanh Trieu <zpeng2k@gmail.com>
2022-07-18 20:27:17 +0700

    Netflix-home-OK

--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,8 @@ const requests = {
     requestPopular : `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`,
     requestTopRated : `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`,
     requestTrending : `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=2`,
-    requestHorror : `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&page=1&include_adult=false`,
+    requestHorror : `
+    https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`,
     requestUpComing : `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`,
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Main from '../Components/Main'
+import Row from '../Components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
         <Main />
+        <Row rowId='1' title='Up Coming' fetchURL={requests.requestUpComing} />
+        <Row rowId='2' title='Trending' fetchURL={requests.requestTrending} />
+        <Row rowId='3' title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row rowId='4' title='Popular' fetchURL={requests.requestPopular} />
+        <Row rowId='5' title='Horror' fetchURL={requests.requestHorror} />
+
     </>
   )
 }
